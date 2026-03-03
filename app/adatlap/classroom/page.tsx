359efecde54faaaa43056470ac00a15eb4343999
PB <pbarna389@gmail.com>
2026-03-03 01:25:40 +0100

    12/classroom - added missing font styles

--- app/adatlap/classroom/page.tsx
+++ app/adatlap/classroom/page.tsx
@@ -26,8 +26,12 @@ export default function Home() {
 
 	return (
 		<main className="flex flex-col justify-center items-center p-28 pl-5 pr-5 w-full gap-5 bg-white font-sans text-black sm:pl-15 sm:pr-15 md:pl-20 md:pr-20 lg:pr-41 lg:pl-41 *:font-montserrat">
-			<h1 className="text-5xl capitalize font-bold">{title.text}</h1>
-			<p className="whitespace-pre-wrap text-center">{content.textContent.text}</p>
+			<h1 className="text-3xl capitalize text-center font-bold basis-3/4 relative sm:text-4xl md:text-5xl lg:text-[58px]">
+				{title.text}
+			</h1>
+			<p className="whitespace-pre-wrap text-center mb-5 text-[18px]">
+				{content.textContent.text}
+			</p>
 			<div
 				className={`space-y-4 text-center transition-opacity duration-500 ${loaded ? 'opacity-100' : 'opacity-0'}`}
 			>
