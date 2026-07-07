3f51ca101e094a6a6605e0df9aa7f2f09464c5fe
PB <pbarna389@gmail.com>
2026-07-07 15:08:52 +0200

    17/articles - added the main constant content

---
+++ app/articles/constants/constants.ts
@@ -0,0 +1,35 @@
+const ARTICLE_FILTER_CATEGORIES = [
+	{
+		id: 'art-filter-theory',
+		text: 'zeneelmélet'
+	},
+	{
+		id: 'art-filter-advices',
+		text: 'tippek'
+	},
+	{
+		id: 'art-filter-int-facts',
+		text: 'érdekességek'
+	},
+	{
+		id: 'art-filter-music',
+		text: 'zenékről'
+	},
+	{
+		id: 'art-filter-filosophy',
+		text: 'filozófia'
+	},
+	{
+		id: 'art-filter-all',
+		text: 'összes cikk'
+	}
+]
+
+export const ARTICLE_MAIN_CONTENT = {
+	title: 'cikkek / zeneblog',
+	textContent: {
+		text:
+			"Folyamatosan bővülő zenei cikkek, gyakorlási tippek, érdekességek, zeneajánlók - témakörökre bontva.Ugyanitt elérhetők az 'Zeneelméleti jajdejók' írásos verziói is.\n(A cikkek tartalmának, anyagainak más helyeken való megjelenítéséhez engedélykérés szükséges!)"
+	},
+	filters: ARTICLE_FILTER_CATEGORIES
+}
