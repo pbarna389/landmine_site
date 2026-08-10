d99c877ac806510753f7554d97c0f6718729574f
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2026-08-10 17:06:35 +0200

    19/videoLayoutFix - moved back the related markup and component tree back to video/page from video/layout (#30)

--- app/videos/layout.tsx
+++ app/videos/layout.tsx
@@ -1,20 +1,7 @@
-import { Suspense } from 'react'
-
-import { LoadingAnimation } from '@/components'
-
-import { VideoAdvertSection, VideoMainSection } from './sections'
-
 export default function VideosLayout({ children }: React.PropsWithChildren) {
 	return (
 		<main className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-raleway *:text-black">
-			<VideoMainSection>
-				<Suspense fallback={<LoadingAnimation />}>{children}</Suspense>
-			</VideoMainSection>
-			<section className="flex flex-col gap-5 p-16 justify-center items-center bg-video-advert w-full *:text-white *:text-center *:font-montserrat">
-				<Suspense fallback={<LoadingAnimation />}>
-					<VideoAdvertSection />
-				</Suspense>
-			</section>
+			{children}
 		</main>
 	)
 }
