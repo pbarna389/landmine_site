455dde66b00065ce73254091874161ccf552a5f6
PB <pbarna389@gmail.com>
2026-08-10 18:54:26 +0200

    21/navbarRouteHighlight - removed redundancies

--- components/Header/components/MobileLink/MobileLink.tsx
+++ components/Header/components/MobileLink/MobileLink.tsx
@@ -13,13 +13,12 @@ type MobileLinkProps = {
 export const MobileLink = ({ link, callback }: MobileLinkProps) => {
 	const pathname = usePathname()
 
-	const isCurrentActive = pathname.length && pathname.includes(link.href)
+	const isCurrentActive = pathname === link.href || pathname.startsWith(`${link.href}`)
 
 	return (
 		<Link
 			href={link.href}
 			className={`top-0 font-raleway p-1 pl-0 pr-0 bg-header-mobile-bg text-black capitalize border border-header-mobile-border w-full cursor-pointer text-center transition-all duration-50 active:scale-105 ${isCurrentActive ? 'scale-115 brightness-115' : ''}`}
-			key={`nav-mobile-main-links-${link.text}`}
 			onClick={callback}
 		>
 			{link.text}
