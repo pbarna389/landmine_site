f612b9345ad579871f24a2f30d48145db7979caa
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2026-09-03 00:01:33 +0200

    25/carousel upgrade (#36)
    * 25/carouselUpgrade - done
    
    * 25/carouselUpgrade - upgrade done
    
    * 25/carouselUpgrade - final push done

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import {
 	HeroCarouselList,
 	HeroCarouselMainText,
+	HeroCarouselSelector,
 	HeroCarouselTextParent
 } from './components'
 import { useCarousel } from '@/hooks/useCarousel'
@@ -12,7 +13,7 @@ import { HERO_DATA } from './constants/constants'
 import { BASE_BASE_ANIM_TIMEOUT, BASE_TIMEOUT } from '@/constants/constants'
 
 export default function Hero() {
-	const { idx, loaded, setLoaded } = useCarousel(
+	const { idx, nextIdx, loaded, setLoaded, changeCurrentSlide } = useCarousel(
 		HERO_DATA.length,
 		BASE_BASE_ANIM_TIMEOUT,
 		BASE_TIMEOUT
@@ -21,24 +22,34 @@ export default function Hero() {
 	const selectedData = HERO_DATA[idx]
 
 	return (
-		<div
-			className={`w-full h-screen relative z-1 transition-all duration-500 shadow-2xl ${loaded ? 'opacity-100' : 'opacity-0'}`}
-		>
-			<Image
-				className="fill object-cover h-full w-full overflow-clip"
-				src={selectedData.img}
-				key={`hero-image-${selectedData.img}`}
-				alt={`Hero Image ${idx}`}
-				priority
-				onLoad={() => setLoaded(true)}
-				onError={() => setLoaded(true)}
+		<div className="w-full h-full">
+			<div
+				className={`w-full h-screen relative z-1 transition-all duration-500 shadow-2xl ${loaded ? 'opacity-100' : 'opacity-0'}`}
+			>
+				<Image
+					className="fill object-cover h-full w-full overflow-clip"
+					src={selectedData.img}
+					key={`hero-image-${selectedData.img}`}
+					alt={`Hero Image ${idx}`}
+					priority
+					onLoad={() => setLoaded(true)}
+					onError={() => setLoaded(true)}
+				/>
+				<HeroCarouselTextParent loadState={loaded} position="top">
+					<HeroCarouselMainText mainText={selectedData.mainText} />
+				</HeroCarouselTextParent>
+				<HeroCarouselTextParent loadState={loaded} position="middle">
+					<HeroCarouselList subText={selectedData.subText} />
+				</HeroCarouselTextParent>
+			</div>
+			<HeroCarouselSelector
+				carouselTimer={BASE_TIMEOUT}
+				callback={changeCurrentSlide}
+				idx={idx}
+				nextIdx={nextIdx}
+				loaded={loaded}
+				length={HERO_DATA.length}
 			/>
-			<HeroCarouselTextParent loadState={loaded} position="top">
-				<HeroCarouselMainText mainText={selectedData.mainText} />
-			</HeroCarouselTextParent>
-			<HeroCarouselTextParent loadState={loaded} position="middle">
-				<HeroCarouselList subText={selectedData.subText} />
-			</HeroCarouselTextParent>
 		</div>
 	)
 }
--- hooks/useCarousel.ts
+++ hooks/useCarousel.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 export const useCarousel = (
 	length: number,
@@ -6,24 +6,70 @@ export const useCarousel = (
 	intervalTimeout: number
 ) => {
 	const [idx, setIdx] = useState(0)
+	const [nextIdx, setNextIdx] = useState(0)
 	const [loaded, setLoaded] = useState(false)
 
-	useEffect(() => {
-		let timeoutId: NodeJS.Timeout | null = null
+	const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+	const intervalRef = useRef<NodeJS.Timeout | null>(null)
 
-		const intervalId = setInterval(() => {
+	const clearTimeouts = useCallback(() => {
+		if (intervalRef.current !== null) {
+			clearInterval(intervalRef.current)
+			intervalRef.current = null
+		}
+		if (timeoutRef.current !== null) {
+			clearTimeout(timeoutRef.current)
+			timeoutRef.current = null
+		}
+	}, [])
+
+	const moveSlideForward = useCallback(() => {
+		intervalRef.current = setInterval(() => {
 			setLoaded(false)
+			setNextIdx((prev) => (prev + 1) % length)
 
-			timeoutId = setTimeout(() => {
+			timeoutRef.current = setTimeout(() => {
 				setIdx((prev) => (prev + 1) % length)
 			}, forwardTimeout)
 		}, intervalTimeout)
+	}, [forwardTimeout, intervalTimeout, length])
 
-		return () => {
-			clearInterval(intervalId)
-			if (timeoutId !== null) clearTimeout(timeoutId)
-		}
-	}, [length, forwardTimeout, intervalTimeout])
+	const selectNextSlide = useCallback(() => {
+		clearTimeouts()
+		moveSlideForward()
+	}, [clearTimeouts, moveSlideForward])
 
-	return { idx, loaded, setLoaded }
+	const changeCurrentSlide = useCallback(
+		(newIdx: number) => {
+			if (idx === newIdx || nextIdx === newIdx) return
+
+			clearTimeouts()
+
+			setLoaded(false)
+			setNextIdx(newIdx)
+
+			timeoutRef.current = setTimeout(() => {
+				setIdx(newIdx)
+
+				moveSlideForward()
+			}, forwardTimeout)
+		},
+		[clearTimeouts, forwardTimeout, idx, moveSlideForward, nextIdx]
+	)
+
+	useEffect(() => {
+		moveSlideForward()
+
+		return () => clearTimeouts()
+	}, [moveSlideForward, clearTimeouts])
+
+	return {
+		idx,
+		nextIdx,
+		loaded,
+		setLoaded,
+		moveSlideForward,
+		selectNextSlide,
+		changeCurrentSlide
+	}
 }
