88140282027ba717343dab975b18fbe30bbefb2e
PB <pbarna389@gmail.com>
2026-09-02 21:39:11 +0200

    24/modal - final commit, done for today

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -8,7 +8,7 @@ const ModalClassNames: { [K in ModalClassNames]: string } = {
 	default:
 		'modal fixed opacity-0 open:opacity-100 transition-opacity w-screen h-screen m-0 p-1 border-0 max-w-none max-h-none',
 	music:
-		'backdrop:bg-transparent open:backdrop-blur-xl brightness-125 top-1/2 left-1/2 -translate-1/2 bg-transparent'
+		'backdrop:bg-transparent open:backdrop:bg-black/75 open:backdrop-blur-xl brightness-125 top-1/2 left-1/2 -translate-1/2 bg-transparent'
 }
 
 type ModalProps = React.PropsWithChildren & {
